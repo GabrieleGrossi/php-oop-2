71e535a1b5756f875a564e684d47642be5aaabff
GabrieleGrossi <grossigabriele98@gmail.com>
2023-07-14 15:44:29 +0200

    Import Bootstrap and create a card

--- Index.php
+++ Index.php
@@ -15,9 +15,32 @@ include __DIR__.'/Product/Products.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>PHP-OOP-2</title>
 </head>
 <body>
-    
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">
+        Card title
+    </h5>
+    <p class="card-text">
+        Some quick example text to build on the card title and make up the bulk of the card's content.
+    </p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">
+        An item
+    </li>
+    <li class="list-group-item">
+        A second item
+    </li>
+    <li class="list-group-item">
+        A third item
+    </li>
+  </ul>
+</div>
 </body>
 </html>
